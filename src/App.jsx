7bd62b86f21b61424403be0a0e26c63ad2afa35c
seenurjpt <seenurjpt1402@gmail.com>
2025-05-15 21:45:35 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="h-screen flex justify-center align-middle text-pink-700">Good</div>
+    </>
+  );
+}
+
+export default App;
