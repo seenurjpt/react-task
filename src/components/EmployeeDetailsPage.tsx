59e0cea4d04451160bced42f397e7d43c6f07211
seenurjpt <seenurjpt1402@gmail.com>
2025-05-15 23:15:58 +0530

    conflict

--- src/components/EmployeeDetailsPage.tsx
+++ src/components/EmployeeDetailsPage.tsx
@@ -7,7 +7,7 @@ const EmployeeDetailsPage = () => {
   return (
     <>
       <EmployeeCard />
-      <div className="flex flex-col md:flex-row md:space-x-6 lg:space-x-8 p-4 md:p-6 lg:p-8">
+      <div className="flex flex-col md:flex-row md:space-x-6 lg:space-x-8">
 
       <div className="w-full md:w-[70%] mb-6 md:mb-0"> {/* Added margin-bottom for mobile stacking */}
         <EmployeeTabs />
